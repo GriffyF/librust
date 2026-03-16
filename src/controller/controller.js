9537359d6d11ab2d71d7cf2553b95f5c199ed599
GriffyF <164791869+GriffyF@users.noreply.github.com>
2026-03-16 12:54:38 -0500

    clear

--- src/controller/controller.js
+++
@@ -1,8 +0,0 @@
-//RENAME THIS FILE AS SOON AS FUNCTIONALITY IS ADDED
-//controllers call Module functions to retreive data from DB
-//handles logic related to that data
-//i.e. if searching for a user, it calls user module to find data from DB
-//  then it handles the logic based on the data
-//      (such as the case for if the data exists or not)
-//controller then delivers the result
-
